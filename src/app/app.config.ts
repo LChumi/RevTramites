70c9f5bb8bb771540012a51684dd440c2718f98e
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-02-20 12:54:05 -0500

    feat: Added providers to httpClient, MessageService, ConfirmationService and import Providers to ToastModule

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {providePrimeNG} from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import {provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
+import {ConfirmationService, MessageService} from 'primeng/api';
+import {ToastModule} from 'primeng/toast';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(withFetch(), withInterceptors([])),
+    MessageService,
+    ConfirmationService,
+    importProvidersFrom(ToastModule),
     providePrimeNG({
       theme: {
         preset: Aura
