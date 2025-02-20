bf4c586e4888f776242a27c71f642669fb3602da
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-02-20 15:51:55 -0500

    feat:(added routes and login compoenent)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import * as path from 'node:path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'icep',
+    children: [
+      {
+        path: 'auth',
+        children: [
+          {path: 'login', loadComponent: () => import('@features/auth/login/login.component')}
+        ]
+      },
+    ]
+  }
+];
