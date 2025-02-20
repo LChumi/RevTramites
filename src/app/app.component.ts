f08f6a7d6cc6f53bad01025e13fbac0d9297d264
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-02-20 11:45:49 -0500

    feat: added primeng primeng themes and transale core

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {PrimeNG} from 'primeng/config';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'RevTramites';
+export class AppComponent implements OnInit {
+
+  private primeng = inject(PrimeNG)
+  private transaletService = inject(TranslateService)
+
+  ngOnInit() {
+    this.primeng.ripple.set(true)
+    this.transaletService.setDefaultLang('es')
+  }
+
+  translate(lang: string){
+    this.transaletService.use(lang);
+    this.transaletService.use('primeng').subscribe(res => this.primeng.setTranslation(res));
+  }
 }
