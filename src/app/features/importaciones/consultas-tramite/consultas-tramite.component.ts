45655ef95aa6ed0ee5594c73bf5237f7a7ffa7f5
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-02-21 17:38:56 -0500

    feat: added new components to searchs

---
+++ src/app/features/importaciones/consultas-tramite/consultas-tramite.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './consultas-tramite.component.html',
+  styles: ``
+})
+export class ConsultasTramiteComponent {
+
+}
