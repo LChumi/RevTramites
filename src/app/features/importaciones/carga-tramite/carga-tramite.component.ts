c9af8c0c036a7207f0b5252671339176ee8879e0
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-02-21 13:03:16 -0500

    feat: added styles in html

--- src/app/features/importaciones/carga-tramite/carga-tramite.component.ts
+++ src/app/features/importaciones/carga-tramite/carga-tramite.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FileUploadModule} from 'primeng/fileupload';
+import {MessageService} from 'primeng/api';
+import {FormsModule} from '@angular/forms';
+import {InputTextModule} from 'primeng/inputtext';
 
 @Component({
-  imports: [],
+  imports: [
+    FileUploadModule,
+    FormsModule,
+    InputTextModule
+  ],
   templateUrl: './carga-tramite.component.html',
   standalone: true,
   styles: ``
 })
 export default class CargaTramiteComponent {
 
+  private messageService = inject(MessageService)
+  protected uploadedFiles: any[] = [] ;
+  protected observacion: any;
+
+  onUpload(event: any) {
+    for (const file of event.files) {
+      this.uploadedFiles.push(file);
+    }
+
+    this.messageService.add({ severity: 'info', summary: 'Exito', detail: 'Archivo registrado y guardado con exito' });
+  }
 }
