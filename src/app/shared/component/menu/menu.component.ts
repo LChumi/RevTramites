54a344695c0f4fc884008d3b5b1cbb40daccfd16
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-03-05 15:28:05 -0500

    fix:changes in menu items

--- src/app/shared/component/menu/menu.component.ts
+++ src/app/shared/component/menu/menu.component.ts
@@ -18,52 +18,61 @@ export class MenuComponent implements OnInit{
     this.model = [
       {
         label: 'Tramites',
-        icon: 'pi pi-fw pi-check-square',
+        icon: 'pi pi-th-large',
         items: [
           {
-            label: 'Carga de Tramites por llegar',
-            icon: 'pi pi-upload',
-            routerLink: '/icep/bodega-recepcion/carga-bultos',
+            label: 'Carga Tramites',
+            icon: 'pi pi-th-large',
+            route: ['/'],
+            items: [
+              {
+                label: 'Carga de Tramites por llegar',
+                icon: 'pi pi-upload',
+                routerLink: '/icep/bodega-recepcion/carga-bultos',
+              },
+              {
+                label: 'Consultas',
+                icon: 'pi pi-fw pi-list',
+                routerLink: '/icep/bodega-recepcion/consulta-tramite',
+              }
+            ]
           },
           {
-            label: 'Consultas',
-            icon: 'pi pi-fw pi-list',
-            routerLink: '/icep/bodega-recepcion/consulta-tramite',
-          }
+            label: 'Revision',
+            icon: 'pi pi-th-large',
+            route: ['/'],
+            items: [
+              {
+                label: 'Revision de productos',
+                icon: 'pi pi-barcode',
+                routerLink: '/icep/bodega-recepcion/revision-bultos',
+              },
+              {
+                label: 'Consultas',
+                icon: 'pi pi-fw pi-list',
+                routerLink: '/icep/bodega-recepcion/consulta-revision',
+              }
+            ]
+          },
+          {
+            label: 'Muestras',
+            icon: 'pi pi-th-large',
+            route: ['/'],
+            items: [
+              {
+              label: 'Muestras por llegar',
+              icon: 'pi pi-window-maximize',
+              routerLink: '/icep/bodega-recepcion/muestras',
+            },
+              {
+                label: 'Consultas',
+                icon: 'pi pi-fw pi-list',
+                routerLink: '/icep/bodega-recepcion/consulta-muestras',
+              }
+              ]
+          },
         ]
       },
-      {
-        label: 'Revision tramites',
-        icon: 'pi pi-fw pi-check-square',
-        items: [
-          {
-            label: 'Revision de productos',
-            icon: 'pi pi-barcode',
-            routerLink: '/icep/bodega-recepcion/revision-bultos',
-          },
-          {
-            label: 'Consultas',
-            icon: 'pi pi-fw pi-list',
-            routerLink: '/icep/bodega-recepcion/consulta-revision',
-          }
-        ]
-      },
-      {
-        label: 'Muestras',
-        icon: 'pi pi-fw pi-check-square',
-        items: [
-          {
-            label: 'Muestras por llegar',
-            icon: 'pi pi-window-maximize',
-            routerLink: '/icep/bodega-recepcion/muestras',
-          },
-          {
-            label: 'Consultas',
-            icon: 'pi pi-fw pi-list',
-            routerLink: '/icep/bodega-recepcion/consulta-muestras',
-          }
-        ]
-      }
     ]
   }
 
