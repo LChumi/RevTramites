0ec8427597a0dfcbb1630f9d985f40b35c228326
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-03-06 12:59:14 -0500

    feat: added methods to search and list in tables

--- src/app/features/importaciones/muestras/consultas-muestras/consultas-muestras.component.ts
+++ src/app/features/importaciones/muestras/consultas-muestras/consultas-muestras.component.ts
@@ -1,11 +1,77 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {MessageService, PrimeTemplate} from "primeng/api";
+import {TableModule} from "primeng/table";
+import {ButtonDirective} from 'primeng/button';
+import {TooltipModule} from 'primeng/tooltip';
+import {InputTextModule} from 'primeng/inputtext';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MuestraService} from '@services/muestra.service';
+import {Muestra} from '@models/muestra';
+import {InputGroupModule} from 'primeng/inputgroup';
+import {NgStyle} from '@angular/common';
+import {ErrorResponse} from '@dtos/error-response';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [
+    TableModule,
+    TooltipModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    FormsModule,
+    InputGroupModule,
+    ButtonDirective,
+    NgStyle
+  ],
   templateUrl: './consultas-muestras.component.html',
   styles: ``
 })
 export default class ConsultasMuestrasComponent {
 
+  private muestraService = inject(MuestraService)
+  private messageService= inject(MessageService);
+  tramite: string ='';
+  muestras: Muestra[] = [];
+  loading = false;
+
+  buscarMuestras() {
+    if (this.tramite) {
+      this.loading = true;
+      this.muestraService.listarTramite(this.tramite).subscribe({
+        next: (data) => {
+          if (data.length > 0) {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Exito',
+              detail: 'Se econtraron registros '
+            })
+            this.muestras = data;
+            this.loading = false;
+            this.tramite=''
+          }else {
+            this.messageService.add({
+              severity: 'warn',
+              summary: `No existen datos en el tramite ${this.tramite}`,
+            })
+            this.tramite=''
+          }
+        },
+        error: (err : ErrorResponse) => {
+          this.muestras = [];
+          this.loading = false;
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Ocurrio un problema',
+            detail: err.message,
+          })
+        }
+      })
+    } else {
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'Campos vacios',
+        detail: 'Ingrese el tramite a buscar',
+      })
+    }
+  }
 }
--- src/app/features/importaciones/revision/consultas-revision/consultas-revision.component.ts
+++ src/app/features/importaciones/revision/consultas-revision/consultas-revision.component.ts
@@ -1,11 +1,79 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {ButtonDirective} from 'primeng/button';
+import {InputGroupModule} from 'primeng/inputgroup';
+import {InputTextModule} from 'primeng/inputtext';
+import {MessageService, PrimeTemplate} from 'primeng/api';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TableModule} from 'primeng/table';
+import {Revision} from '@models/revision';
+import {RevisionService} from '@services/revision.service';
+import {ErrorResponse} from '@dtos/error-response';
+import {NgStyle} from '@angular/common';
+import {Ripple} from 'primeng/ripple';
+import {EstadoColorPipe} from '@shared/pipes/estado-color.pipe';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonDirective,
+    InputGroupModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    TableModule,
+    FormsModule,
+    NgStyle,
+    Ripple,
+    EstadoColorPipe
+  ],
   templateUrl: './consultas-revision.component.html',
   styles: ``
 })
 export default class ConsultasRevisionComponent {
 
+  revisionService = inject(RevisionService)
+  messageService = inject(MessageService)
+  tramite: string=''
+  loading: boolean = false;
+  revisiones: Revision[] =[]
+
+  buscarRevisiones(){
+    if (this.tramite) {
+      this.loading = true;
+      this.revisionService.findByTramite(this.tramite).subscribe({
+        next: (data) => {
+          if (data.length > 0) {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Exito',
+              detail: 'Se econtraron registros '
+            })
+            this.revisiones = data;
+            this.loading = false;
+            this.tramite=''
+          }else {
+            this.messageService.add({
+              severity: 'warn',
+              summary: `No existen datos en el tramite ${this.tramite}`,
+            })
+            this.tramite=''
+          }
+        },
+        error: (err : ErrorResponse) => {
+          this.revisiones = [];
+          this.loading = false;
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Ocurrio un problema',
+            detail: err.message,
+          })
+        }
+      })
+    } else {
+      this.messageService.add({
+        severity: 'warn',
+        summary: 'Campos vacios',
+        detail: 'Ingrese el tramite a buscar',
+      })
+    }
+  }
 }
