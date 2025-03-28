9e7396748d5913acb3ce98b05b750d1fd6e81fb2
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2025-03-28 18:23:31 -0500

    fix: added attributes and update html changes in styles

--- src/app/core/models/contenedor.ts
+++ src/app/core/models/contenedor.ts
@@ -1,6 +1,9 @@
+import {Producto} from '@models/producto';
+
 export interface Contenedor {
   id:         string;
   usrBloquea: string;
   bloqueado:  boolean;
   finalizado: boolean;
+  productos:  Producto[];
 }
