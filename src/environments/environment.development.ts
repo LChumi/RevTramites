d4f8e5e1b0c9bc380ad397562c93a3f75b20418a
LChumi <luischumi.9@gmail.com>
2026-05-11 17:26:25 -0500

    style: changes into styles by p-table

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  apiUrlBase: `http://127.0.0.1:7572/`,
-  apiLogin: `http://127.0.0.1:7569/`,
+  apiUrlBase: `http://192.168.112.192:7572/`,
+  apiLogin: `http://192.168.112.192:7569/`,
   imagesUrl: `https://apis.cumpleanos.com.ec/assist/images/producto`,
   production: false
 };
