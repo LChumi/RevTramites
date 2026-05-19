53c2d8b4b8e05e28d5661efeb8fffa1f2f375d1e
LChumi <luischumi.9@gmail.com>
2026-05-19 17:57:15 -0500

    fix: changes into finalizacion service and update services finsih proces to revision (#10)

--- src/app/features/recepcion-almacenes/recepcion-almacenes.component.ts
+++ src/app/features/recepcion-almacenes/recepcion-almacenes.component.ts
@@ -1,21 +1,13 @@
 import {Component, inject, OnInit} from '@angular/core';
-import {ButtonDirective} from 'primeng/button';
-import {Ripple} from 'primeng/ripple';
 import {CheckboxModule} from 'primeng/checkbox';
-import {NgClass} from '@angular/common';
 import {AvatarGroupModule} from 'primeng/avatargroup';
 import {AvatarModule} from 'primeng/avatar';
 import {MenuModule} from 'primeng/menu';
 import {DropdownModule} from 'primeng/dropdown';
-import {BodegaService} from '@services/bodega.service';
-import {Bodega} from '@dtos/bodega';
 import {FormsModule} from '@angular/forms';
-import {RecepcionAlmacenesService} from '@services/recepcion-almacenes.service';
 import {ProgressSpinnerModule} from 'primeng/progressspinner';
 import {CardModule} from 'primeng/card';
 import {TableModule} from 'primeng/table';
-import {Comprobantes} from '@dtos/recepcion-almacenes/comprobantes';
-import {RecepcionSidebarComponent} from '@features/recepcion-almacenes/recepcion-sidebar/recepcion-sidebar.component';
 
 @Component({
   selector: 'app-recepcion-almacenes',
@@ -29,8 +21,7 @@ import {RecepcionSidebarComponent} from '@features/recepcion-almacenes/recepcion
     FormsModule,
     ProgressSpinnerModule,
     CardModule,
-    TableModule,
-    RecepcionSidebarComponent
+    TableModule
   ],
   templateUrl: './recepcion-almacenes.component.html',
   styles: ``
