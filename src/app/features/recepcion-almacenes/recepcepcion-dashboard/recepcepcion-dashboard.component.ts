8bb217df57b78529e70066ebb65bab9cfcb42fb7
LChumi <luischumi.9@gmail.com>
2026-05-21 16:53:40 -0500

    feat: added new components (#1)

---
+++ src/app/features/recepcion-almacenes/recepcepcion-dashboard/recepcepcion-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recepcepcion-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './recepcepcion-dashboard.component.html',
+  styles: ``
+})
+export class RecepcepcionDashboardComponent {
+
+}
